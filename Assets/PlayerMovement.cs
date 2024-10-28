2d6f2447c75e98397d3f3d5572bda4cd5eb94433
alemt19 <alfredomt1906@gmail.com>
2024-10-28 09:24:50 -0400

    Cuerda, jalar cuerda y movimiento

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -34,7 +34,7 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         // Movement
-        rb.velocity = (currentMovementSpeed) * lastMovement;
+        rb.AddForce((currentMovementSpeed) * lastMovement);
     }
 
     private void Player1Movement()
@@ -83,7 +83,7 @@ public class PlayerMovement : MonoBehaviour
 
         if (movement.magnitude > 0)
         {
-            lastMovement = movement;
+            lastMovement = movement.normalized;
         }
 
         animator.SetFloat("Speed", movement.sqrMagnitude);
@@ -131,6 +131,11 @@ public class PlayerMovement : MonoBehaviour
         if (movement.magnitude > 1)
         {
             movement.Normalize();
+        }
+
+        if (movement.magnitude > 0)
+        {
+            lastMovement = movement.normalized;
         }
 
         animator.SetFloat("Speed", movement.sqrMagnitude);
---
+++ Assets/PlayerRope.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerRope : MonoBehaviour
+{
+    GameObject nave1;
+    public Rigidbody2D rb;
+    public float mult = 10f;
+    public PlayerMovement playerMovementReference;
+    float duration;
+    float baseDuration = 0.2f;
+    float elapsedTime;
+    Vector3 direction;
+    void Start()
+    {
+        nave1 = GameObject.FindGameObjectWithTag("Nave1");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && elapsedTime >= duration)
+        {
+            elapsedTime = 0f;
+            duration = (nave1.transform.position - transform.position).magnitude * baseDuration;
+            Debug.Log(duration);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            direction = nave1.transform.position - transform.position;
+            rb.AddForce(direction.normalized * mult);
+        }
+    }
+}
